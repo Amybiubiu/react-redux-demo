dc11c97873844c33d4475d5e34632ef07e25be55
Amybiubiu <1246457100@qq.com>
2020-11-19 21:37:58 +0800

    the part of code which is about store ,reducer, action'

---
+++ src/app/reducer.js
@@ -0,0 +1,35 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers(todos, visibilityFilter);
+function todos(state = [], action){
+    switch(action.type){
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    text: action.text,
+                    completed: false,
+                }
+            ];
+        case 'COMPLETE_TODO':
+            return [
+                ...state.slice(0, action.index),
+                Object.assign({}, state[action.index], {
+                    completed: !state[action.index].completed,
+                }),
+                ...state.slice(action.index + 1)
+            ]
+        default:
+            return state;
+    }
+}
+
+function visibilityFilter(state = 'SHOW_ALL', action){
+    switch(action.type){
+        case 'SET_VISIBILITY_FITER':
+            return action.filter;
+        default:
+            return state;
+    }
+}
+export default rootReducer;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+export default function configureStore(initialState) {
+  const store = createStore(rootReducer, initialState);
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers????
+    module.hot.accept('./reducers', () => {
+      const nextReducer = require('./reducers');
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  return store;
+}
